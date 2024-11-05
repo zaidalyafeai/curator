411a873907bd0ee41f78121a5b01501f73d66d0e
Trung Vu <vu.trung.96@gmail.com>
2024-11-05 20:54:36 +0000

    remove unnecessary code

--- bella.py
+++ bella.py
@@ -6,11 +6,9 @@ import json
 import logging
 import os
 import time
-from concurrent.futures import ProcessPoolExecutor
 from typing import Iterable, Optional
 
 from datasets import Dataset
-from datasets.arrow_writer import ArrowWriter
 from xxhash import xxh64
 
 from api_request_parallel_processor import process_api_requests_from_file
@@ -52,54 +50,6 @@ def _create_requests_file(
                     request = prompter.get_request_object(sample, idx)
                     f.write(json.dumps(request) + "\n")
         logging.info(f"Requests file {requests_file} written to disk.")
-
-
-def _read_responses_file_and_write_to_dataset(
-    prompter: Prompter, responses_file, dataset_file, output_column
-):
-    total_count = 0
-    failed_count = 0
-    with ArrowWriter(path=dataset_file) as writer:
-        with open(responses_file, "r") as f_in:
-            for line in f_in:
-                total_count += 1
-                try:
-                    response = json.loads(line)
-                    if isinstance(response[1], list):
-                        # A failed requests contains a list of all the errors before max_retries
-                        logging.info(
-                            f"Request {response[2].get('request_idx')} failed due to errors: {response[1]}"
-                        )
-                        failed_count += 1
-                        continue
-
-                    metadata = response[2]
-                    response_message = response[1]["choices"][0]["message"]["content"]
-
-                    # Parse the response for structured output
-                    if prompter.response_format:
-                        response_parsed = prompter.response_format.model_validate_json(
-                            response_message
-                        )
-                        response_parsed = response_parsed.model_dump()
-                    else:
-                        response_parsed = response_message
-
-                    sample = metadata["sample"]
-
-                    sample[output_column] = response_parsed
-                    if sample == None:
-                        raise ValueError("Trying to write a None sample")
-                    writer.write(sample)
-
-                except Exception as e:
-                    logging.warning(f"Error: {e}")
-                    logging.warning(f"Full response: {response}")
-                    continue
-        logging.debug(f"Read {total_count} responses, {failed_count} failed")
-        if failed_count == total_count:
-            raise ValueError("All requests failed")
-        writer.finalize()
 
 
 def _parse_responses_file(prompter: Prompter, responses_file):
@@ -145,12 +95,18 @@ def _parse_responses_file(prompter: Prompter, responses_file):
         raise ValueError("All requests failed")
     return samples
 
+
 def _hash_dataset(dataset: Iterable):
     """Hash a dataset to a consistent value using parallel processing."""
     start = time.perf_counter_ns()
-    hash = xxh64("|||".join([json.dumps(row, sort_keys=True) for row in dataset])).hexdigest()
-    logging.debug(f"Dataset hash time: {(time.perf_counter_ns() - start) / 1e9:.2f} seconds")
+    hash = xxh64(
+        "|||".join([json.dumps(row, sort_keys=True) for row in dataset])
+    ).hexdigest()
+    logging.debug(
+        f"Dataset hash time: {(time.perf_counter_ns() - start) / 1e6:.6f} miliseconds"
+    )
     return hash
+
 
 def completions(
     dataset: Iterable,
