a9955e34b10073c28089e1d33392e78d136539ab
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2024-11-06 03:13:36 +0000

    better component for tooltip

--- bespoke-dataset-viewer/components/ui/tooltip.tsx
+++ bespoke-dataset-viewer/components/ui/tooltip.tsx
@@ -13,11 +13,10 @@ const TooltipTrigger = TooltipPrimitive.Trigger
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
->(({ className, sideOffset = 4, delayDuration = 100, ...props }, ref) => (
+>(({ className, sideOffset = 4, ...props }, ref) => (
   <TooltipPrimitive.Content
     ref={ref}
     sideOffset={sideOffset}
-    delayDuration={delayDuration}
     className={cn(
       "z-50 overflow-hidden rounded-md bg-popover px-3 py-2 text-sm text-popover-foreground shadow-md animate-in fade-in-50 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
       className
