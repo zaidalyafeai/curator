d82dd51fa4115c275a8370a62a62a7c6cbe1951e
Trung Vu <vu.trung.96@gmail.com>
2024-11-12 22:51:59 +0000

    simplify the non-running loop flow

--- src/bespokelabs/curator/request_processor/event_loop.py
+++ src/bespokelabs/curator/request_processor/event_loop.py
@@ -15,9 +15,6 @@ def run_in_event_loop(coroutine):
     except RuntimeError as e:
         # If no event loop is running, asyncio will
         # return a RuntimeError (https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.get_running_loop).
-        # In that case, we can create a new event loop.
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        result = loop.run_until_complete(coroutine)
-        loop.close()
-        return result
+        # In that case, we can just use asyncio.run.
+        return asyncio.run(coroutine)
+
