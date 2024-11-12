88cdb348160391a30c63f8c823f2da64d54298e4
Mahesh Sathiamoorthy <mahesh@bespokelabs.ai>
2024-11-12 01:31:47 +0000

    Minor cleanup

--- examples/poem.py
+++ examples/poem.py
@@ -59,7 +59,6 @@ from typing import List
 
 
 # We use Pydantic and structured outputs to define the format of the response.
-# This defines a single topic.
 
 # This defines a list of topics, which is the response format for the topic generator.
 class Topics(BaseModel):
