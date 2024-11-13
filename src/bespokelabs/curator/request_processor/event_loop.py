deef5526541703662103999f31e7007017055398
Trung Vu <vu.trung.96@gmail.com>
2024-11-13 00:19:43 +0000

    another event loop fix

--- src/bespokelabs/curator/request_processor/event_loop.py
+++ src/bespokelabs/curator/request_processor/event_loop.py
@@ -8,7 +8,20 @@ def run_in_event_loop(coroutine):
     """
     Run a coroutine in the current event loop or create a new one if there isn't one.
     """
-    nest_asyncio.apply()
+    
+    
+    try:
+        asyncio.get_running_loop()
 
-    return asyncio.run(coroutine)
+        # If there is an event loop running (the call
+        # above doesn't raise an exception), we can
+        # use nest_asyncio to patch the event loop.
+        nest_asyncio.apply()
+
+        return asyncio.run(coroutine)
+    except RuntimeError as e:
+        # If no event loop is running, asyncio will
+        # return a RuntimeError (https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.get_running_loop).
+        # In that case, we can just use asyncio.run.
+        return asyncio.run(coroutine)
 
