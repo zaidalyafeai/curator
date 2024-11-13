0f8247f0311293b39e6f447468d01e1750762563
Trung Vu <vu.trung.96@gmail.com>
2024-11-13 00:17:00 +0000

    use nest_asyncio

--- src/bespokelabs/curator/request_processor/event_loop.py
+++ src/bespokelabs/curator/request_processor/event_loop.py
@@ -1,20 +1,14 @@
 import asyncio
 from time import sleep
 
+import nest_asyncio
+
 
 def run_in_event_loop(coroutine):
     """
     Run a coroutine in the current event loop or create a new one if there isn't one.
     """
-    try:
-        loop = asyncio.get_running_loop()
-        future = loop.create_task(coroutine)
-        while not future.done():
-            sleep(1)
-        return future.result()
-    except RuntimeError as e:
-        # If no event loop is running, asyncio will
-        # return a RuntimeError (https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.get_running_loop).
-        # In that case, we can just use asyncio.run.
-        return asyncio.run(coroutine)
+    nest_asyncio.apply()
+
+    return asyncio.run(coroutine)
 
