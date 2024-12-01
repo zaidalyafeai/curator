10ea1f4e290693ccf13910ac0942975cd255904f
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2024-12-01 21:54:19 +0000

    remove unused unused function that is been refactored

--- src/bespokelabs/curator/request_processor/openai_online_request_processor.py
+++ src/bespokelabs/curator/request_processor/openai_online_request_processor.py
@@ -41,58 +41,6 @@ def api_endpoint_from_url(url: str) -> str:
         return match.group(1)
     return url
 
-
-def num_tokens_consumed_from_request(
-    api_specific_request_json: dict,
-    api_endpoint: str,
-    token_encoding_name: str,
-) -> int:
-    """Count the number of tokens in the request. Only supports completion and embedding requests."""
-    encoding = tiktoken.get_encoding(token_encoding_name)
-
-    if api_endpoint.endswith("completions"):
-        max_tokens = api_specific_request_json.get("max_tokens", 15)
-        n = api_specific_request_json.get("n", 1)
-        completion_tokens = n * max_tokens
-
-        if api_endpoint.startswith("chat/"):
-            num_tokens = 0
-            for message in api_specific_request_json["messages"]:
-                num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
-                for key, value in message.items():
-                    try:
-                        num_tokens += len(encoding.encode(str(value)))
-                    except TypeError:
-                        logger.warning(
-                            f"Failed to encode value {value} with tiktoken to count tokens. Instead assuming a token for every 4 characters."
-                        )
-                        num_tokens += len(str(value)) // 4
-                    if key == "name":  # if there's a name, the role is omitted
-                        num_tokens -= 1  # role is always required and always 1 token
-            num_tokens += 2  # every reply is primed with <im_start>assistant
-            return num_tokens + completion_tokens
-        else:
-            prompt = api_specific_request_json["prompt"]
-            if isinstance(prompt, str):
-                prompt_tokens = len(encoding.encode(prompt))
-                return prompt_tokens + completion_tokens
-            elif isinstance(prompt, list):
-                prompt_tokens = sum([len(encoding.encode(p)) for p in prompt])
-                return prompt_tokens + completion_tokens * len(prompt)
-            else:
-                raise TypeError('Expecting string or list of strings for "prompt" field')
-    elif api_endpoint == "embeddings":
-        input = api_specific_request_json["input"]
-        if isinstance(input, str):
-            return len(encoding.encode(input))
-        elif isinstance(input, list):
-            return sum([len(encoding.encode(i)) for i in input])
-        else:
-            raise TypeError('Expecting string or list of strings for "inputs" field')
-    else:
-        raise NotImplementedError(f'API endpoint "{api_endpoint}" not implemented')
-
-
 class OpenAIOnlineRequestProcessor(OnlineRequestProcessor):
     """OpenAI-specific implementation of the OnlineRequestProcessor.
     
