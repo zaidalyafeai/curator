807632474b8dd40cf118a47a4b4777dec67c7283
Ryan Marten <ryanmarten2000@gmail.com>
2025-01-02 19:04:35 -0800

    trying to add a test for this new functionality

--- tests/batch/test_partial_batch_resubmit.py
+++ tests/batch/test_partial_batch_resubmit.py
@@ -11,9 +11,15 @@ from bespokelabs.curator.request_processor.config import BatchRequestProcessorCo
 from bespokelabs.curator.types.generic_batch import GenericBatch
 from bespokelabs.curator.llm.prompt_formatter import PromptFormatter
 import json
+import shutil
+import os
+from bespokelabs.curator import LLM
+from datasets import Dataset
 
 
-def test_generic_response_file_from_responses(responses):
+def test_generic_response_file_from_responses():
+    with open("tests/batch/msgbatch_01AAs5rns9HhDrWvFxCoivqg_results.jsonl", "r") as f:
+        responses = [json.loads(line) for line in f]
 
     config = BatchRequestProcessorConfig(
         model="claude-3-5-haiku-20241022",
@@ -66,9 +72,30 @@ def test_generic_response_file_from_responses(responses):
     abrp.generic_response_file_from_responses(responses, batch)
 
 
+def test_partial_batch_resubmit():
+    # Clean up test directory if it exists
+    test_dir = "tests/batch/test_working_dir"
+    if os.path.exists(test_dir):
+        shutil.rmtree(test_dir)
+
+    # Create test directory and copy files
+    os.makedirs(test_dir)
+    shutil.copy("tests/batch/requests_0.jsonl", f"{test_dir}/requests_0.jsonl")
+    shutil.copy("tests/batch/batch_objects.jsonl", f"{test_dir}/batch_objects.jsonl")
+
+    llm = LLM(
+        prompt_func=lambda row: row["instruction"],
+        model_name="claude-3-5-haiku-20241022",
+        response_format=None,
+        batch=True,
+        batch_size=50_000,
+        batch_check_interval=60,
+        base_url=None,
+    )
+
+    # this doesn't work because it creates a subdir with the hash in the test_working_dir
+    llm(dataset=Dataset.from_dict({"instruction": ["just say 'hi'"] * 1551}), working_dir=test_dir)
+
+
 if __name__ == "__main__":
-
-    with open("tests/batch/msgbatch_01AAs5rns9HhDrWvFxCoivqg_results.jsonl", "r") as f:
-        responses = [json.loads(line) for line in f]
-
-    test_generic_response_file_from_responses(responses)
+    test_partial_batch_resubmit()
