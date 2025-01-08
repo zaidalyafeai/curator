65892b49dcf2d944bdcdfab130d23f985fd15b0a
vutrung96 <vu.trung.96@gmail.com>
2025-01-07 16:36:17 -0800

    docs: better warning message

--- src/bespokelabs/curator/db.py
+++ src/bespokelabs/curator/db.py
@@ -69,8 +69,7 @@ class MetadataDB:
             cursor = conn.cursor()
             # IMPORTANT: If you modify the CREATE TABLE schema below,
             # you must update the expected_columns list in validate_schema()
-            # to match the new schema. Otherwise, schema validation will fail
-            # and users will need to clear their cache.
+            # to match the new schema. Otherwise, schema validation will fail.
             cursor.execute(
                 """
                 CREATE TABLE IF NOT EXISTS runs (
