be58e142a6551357b0fc9336a63f5fc27999c377
Trung Vu <vu.trung.96@gmail.com>
2025-01-14 22:35:03 +0000

    feat: publish 0.1.15 + better publishing process

---
+++ publish_pkg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Build Curator Viewer
+python build_pkg.py
+
+# Build Curator Package
+poetry build
+
+# Publish Curator Package
+poetry publish
+
+# Clean up
+rm -rf src/bespokelabs/curator/viewer/static
+rm -rf dist
