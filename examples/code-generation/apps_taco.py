4e59e82ecdb4278909062f18343dc6936852f161
Shreyas Pimpalgaonkar <shreyas.gp.7@gmail.com>
2025-01-24 07:02:44 -0800

    checkin changes

--- examples/code-generation/apps_taco.py
+++ examples/code-generation/apps_taco.py
@@ -1,29 +1,33 @@
-from bespokelabs import curator
 from datasets import load_dataset
+
+from bespokelabs import curator
 from bespokelabs.curator.experimental.code_executor.code_executor import TestCase
 
 dataset = load_dataset("bespokelabs/sky-t1-taco-test-rejection-sampled-shreyas")
 
-dataset['train'][0]
+dataset["train"][0]
+
 
 class APPSCodeExecutor(curator.experimental.CodeExecutor):
-
     def function_name(self, row):
-        return row['function_name']
+        return row["function_name"]
 
     def function_code(self, row):
         import re
-        code = re.search(r"```python\n(.*)\n```", row['deepseek_solution']).group(1)
+
+        code = re.search(r"```python\n(.*)\n```", row["deepseek_solution"]).group(1)
         return code
 
     def test_cases(self, row) -> list[TestCase]:
-        import pdb; pdb.set_trace()
-        return [TestCase(input=row['input_output'][0], expected_output=row['input_output'][1])]
+        import pdb
+
+        pdb.set_trace()
+        return [TestCase(input=row["input_output"][0], expected_output=row["input_output"][1])]
 
     def parse_results(self, row, test_cases, execution_results):
-        return row['test_results']
+        return row["test_results"]
 
 
 executor = APPSCodeExecutor(backend="e2b")
 
-execution_output = executor(dataset['train'].select(range(10)))
+execution_output = executor(dataset["train"].select(range(10)))
--- examples/code-generation/generate.py
+++ examples/code-generation/generate.py
@@ -1,20 +1,22 @@
-# generation of code. generating test cases for the code and evaluating them on those test cases. 
+# generation of code. generating test cases for the code and evaluating them on those test cases.
+
+
+from pydantic import BaseModel, Field
 
 from bespokelabs import curator
-from pydantic import BaseModel, Field
-from typing import List, Dict
-from datasets import load_dataset
-
 from bespokelabs.curator.experimental.code_execution_backend.e2b_backend import E2BCodeExecutionBackend
+
 
 class CodeGenerationOutput(BaseModel):
     code: str = Field(..., description="The generated code")
 
-class PromptConstants:
-    SYSTEM_MESSAGE_GENERIC = f"You are an expert Python programmer. You will be given a question (problem specification) and will generate a correct Python program that matches the specification and passes all tests."
-    FORMATTING_MESSAGE_WITH_STARTER_CODE = "You will use the following starter code to write the solution to the problem and enclose your code within delimiters."
-    FORMATTING_WITHOUT_STARTER_CODE = "Read the inputs from stdin solve the problem and write the answer to stdout (do not directly test on the sample inputs). Enclose your code within delimiters as follows. Ensure that when the python program runs, it reads the inputs, runs the algorithm and writes output to STDOUT."
 
+class PromptConstants:
+    SYSTEM_MESSAGE_GENERIC = "You are an expert Python programmer. You will be given a question (problem specification) and will generate a correct Python program that matches the specification and passes all tests."
+    FORMATTING_MESSAGE_WITH_STARTER_CODE = (
+        "You will use the following starter code to write the solution to the problem and enclose your code within delimiters."
+    )
+    FORMATTING_WITHOUT_STARTER_CODE = "Read the inputs from stdin solve the problem and write the answer to stdout (do not directly test on the sample inputs). Enclose your code within delimiters as follows. Ensure that when the python program runs, it reads the inputs, runs the algorithm and writes output to STDOUT."
 
 
 class SumCodeVerifier(curator.CodeVerifier):
@@ -23,28 +25,27 @@ class SumCodeVerifier(curator.CodeVerifier):
 
     def verify(self, code):
         return self.execute(code) == 100
-        
+
+
 class CodeGenerationLLM(curator.LLM):
     response_format = CodeGenerationOutput
     verifier = SumCodeVerifier()
 
     def prompt(self, row: str):
         prompt = f"### Question:\n{row['question_content']}\n\n"
-        if row['starter_code']:
-            prompt += (
-                f"### Format: {PromptConstants.FORMATTING_MESSAGE_WITH_STARTER_CODE}\n"
-            )
+        if row["starter_code"]:
+            prompt += f"### Format: {PromptConstants.FORMATTING_MESSAGE_WITH_STARTER_CODE}\n"
             prompt += f"```python\n{row['starter_code']}\n```\n\n"
         else:
             prompt += f"### Format: {PromptConstants.FORMATTING_WITHOUT_STARTER_CODE}\n"
             prompt += "```python\n# YOUR CODE HERE\n```\n\n"
         return prompt
 
-
     def parse(self, row, response):
-        row['generated_code'] = response.code
+        row["generated_code"] = response.code
         return row
-    
+
+
 code_generator = CodeGenerationLLM(
     model_name="gpt-4o",
     generation_params={"temperature": 0.2, "max_tokens": 4096},
