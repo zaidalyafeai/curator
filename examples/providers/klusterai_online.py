fc5214f8420c5bad423dc9b2bd6ba0bc24370bae
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2025-01-30 02:09:09 +0000

    fix cost display issues

--- examples/providers/klusterai_online.py
+++ examples/providers/klusterai_online.py
@@ -1,4 +1,24 @@
 from bespokelabs import curator
+import litellm
+
+litellm.suppress_debug_info = True
+
+litellm.register_model(
+    {
+        "klusterai/Meta-Llama-3.1-8B-Instruct-Turbo": {
+            "max_tokens": 8192, 
+            "input_cost_per_token": 0.18 / 1e6,  # source: https://www.kluster.ai/#pricing
+            "output_cost_per_token": 0.18 / 1e6,
+            "litellm_provider": "openai",
+        },
+        "deepseek-ai/DeepSeek-R1": {
+            "max_tokens": 8192,
+            "input_cost_per_token": 2.00 / 1e6,  # source: https://www.kluster.ai/#pricing
+            "output_cost_per_token": 2.00 / 1e6,
+            "litellm_provider": "openai",
+        }
+    }
+)
 
 llm = curator.LLM(model_name="klusterai/Meta-Llama-3.1-8B-Instruct-Turbo", backend="klusterai", backend_params={"max_retries": 1})
 
