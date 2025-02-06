34e3f2a2a18a3d203f691f8ebee1019c11a9fb8d
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2025-02-06 21:20:53 +0000

    rm default logging to avoid duplicate logs

--- src/bespokelabs/__init__.py
+++ src/bespokelabs/__init__.py
@@ -1,11 +1,1 @@
 """BespokeLabs Curator."""
-
-import logging
-
-logger = logging.getLogger("bespokelabs.curator")
-
-handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.WARNING)
