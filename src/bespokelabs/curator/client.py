de1039fe28cf0ddbcc9b340afd342827bdcc2f17
Trung Vu <vu.trung.96@gmail.com>
2025-02-13 04:59:23 +0000

    ref: add logging + change url to prod

--- src/bespokelabs/curator/client.py
+++ src/bespokelabs/curator/client.py
@@ -10,6 +10,7 @@ import requests
 from bespokelabs.curator.constants import BASE_CLIENT_URL, PUBLIC_CURATOR_VIEWER_URL
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class _SessionStatus:
@@ -42,6 +43,8 @@ class Client:
 
     def create_session(self, metadata: t.Dict):
         """Sends a POST request to the server to create a session."""
+        if "HOSTED_CURATOR_VIEWER" not in os.environ:
+            logger.info("Set HOSTED_CURATOR_VIEWER=1 to view your data live at https://curator.bespokelabs.ai/datasets/.")
         if not self.hosted:
             return str(uuid.uuid4().hex)
 
@@ -49,12 +52,11 @@ class Client:
             return self.session
         metadata.update({"status": _SessionStatus.STARTED})
 
-        response = requests.post(f"{BASE_CLIENT_URL}/session", json=metadata)
+        response = requests.post(f"{BASE_CLIENT_URL}/sessions", json=metadata)
 
         if response.status_code == 200:
             self._session = response.json().get("session_id")
-            logger.info(f"Created session: {self.session}")
-            logger.info("Viewer available at: " + f"{PUBLIC_CURATOR_VIEWER_URL}/{self.session}")
+            logger.info("View your data live at: " + f"{PUBLIC_CURATOR_VIEWER_URL}/{self.session}")
             self._state = _SessionStatus.STARTED
             return self.session
         else:
@@ -63,10 +65,7 @@ class Client:
 
     async def _update_state(self):
         async with httpx.AsyncClient() as client:
-            response = await client.put(
-                f"{BASE_CLIENT_URL}/session/{self.session}",
-                json={"status":self._state}
-            )
+            response = await client.put(f"{BASE_CLIENT_URL}/sessions/{self.session}", json={"status": self._state})
             if response.status_code != 200:
                 logger.debug(f"Failed to update session status: {response.status_code}, {response.text}")
 
@@ -85,7 +84,7 @@ class Client:
 
         response_data = json.dumps({"response_data": response_data})
         async with httpx.AsyncClient() as client:
-            response = await client.post(f"{BASE_CLIENT_URL}/{self.session}/stream_response/{idx}", data=response_data)
+            response = await client.post(f"{BASE_CLIENT_URL}/sessions/{self.session}/responses/{idx}", data=response_data)
 
         if response.status_code != 200:
             logger.debug(f"Failed to stream response to curator Viewer: {response.status_code}, {response.text}")
--- src/bespokelabs/curator/constants.py
+++ src/bespokelabs/curator/constants.py
@@ -3,5 +3,5 @@
 BATCH_REQUEST_ID_TAG = "custom_id"
 _CURATOR_DEFAULT_CACHE_DIR = "~/.cache/curator"
 _DEFAULT_CACHE_DIR = "~/.cache"
-BASE_CLIENT_URL = "http://localhost:8000/v0/viewer"
-PUBLIC_CURATOR_VIEWER_URL = "https://curator-viewer-319533213591.us-central1.run.app/dataset/"
+BASE_CLIENT_URL = "https://api.bespokelabs.ai/v0/viewer"
+PUBLIC_CURATOR_VIEWER_URL = "https://curator.bespokelabs.ai/datasets"
