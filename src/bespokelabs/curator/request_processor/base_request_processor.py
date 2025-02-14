5a570bc2ab10676ae31aa0a4a2d9f4766500b701
TheDude <kartik4949@gmail.com>
2025-02-14 22:40:19 +0530

    ref: update online curator client

--- src/bespokelabs/curator/request_processor/base_request_processor.py
+++ src/bespokelabs/curator/request_processor/base_request_processor.py
@@ -411,12 +411,8 @@ class BaseRequestProcessor(ABC):
                             if len(error_sample) < 10:
                                 error_sample.append(str(response.response_errors))
                             continue
-                        if response.parsed_response_message is None:
-                            dataset_rows = self._process_response(response)
-                        else:
-                            dataset_rows = response.parsed_response_message
 
-                        for row in dataset_rows:
+                        for row in response.parsed_response_message:
                             if isinstance(row, BaseModel):
                                 row = row.model_dump()
 
