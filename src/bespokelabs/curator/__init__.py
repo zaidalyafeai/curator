6e2a3088e60e1bef4625634084cfa5d7eb125f5e
TheDude <kartik4949@gmail.com>
2025-02-19 18:46:25 +0530

    ref: update logger in curator init

--- src/bespokelabs/curator/__init__.py
+++ src/bespokelabs/curator/__init__.py
@@ -1,19 +1,8 @@
 """BespokeLabs Curator."""
 
-import logging
-
-from rich.console import Console
-from rich.logging import RichHandler
-
 from .code_executor.code_executor import CodeExecutor
 from .llm.llm import LLM
+from .log import _CONSOLE
 from .types import prompt as types
 
 __all__ = ["LLM", "CodeExecutor", "types"]
-
-_CONSOLE = Console(stderr=True)
-logger = logging.getLogger("bespokelabs.curator")
-
-logger.setLevel(logging.WARNING)
-
-logger.addHandler(RichHandler(console=_CONSOLE, markup=True))
