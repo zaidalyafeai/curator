e19fd518afe2b2394f026ec0e895ceb7da4d96d2
TheDude <kartik4949@gmail.com>
2025-02-20 12:29:33 +0530

    feat: add huggingface url support in push to viewer

--- src/bespokelabs/curator/utils.py
+++ src/bespokelabs/curator/utils.py
@@ -1,9 +1,10 @@
 import asyncio
 import json
 import logging
+import typing as t
 import uuid
 
-from datasets import Dataset
+from datasets import Dataset, load_dataset
 
 from bespokelabs.curator import constants
 from bespokelabs.curator.client import Client, _SessionStatus
@@ -12,16 +13,22 @@ from bespokelabs.curator.request_processor.event_loop import run_in_event_loop
 logger = logging.getLogger(__name__)
 
 
-def push_to_viewer(dataset: Dataset, chunk_size: int = 100):
+def push_to_viewer(dataset: Dataset | str, hf_params: t.Optional[t.Dict] = None, chunk_size: int = 100):
     """Push a dataset to the Curator Viewer.
 
     Args:
-        dataset (Dataset): The dataset to push to the Curator Viewer.
+        dataset (Dataset | str): The dataset to push to the Curator Viewer.
+        hf_params: (dict): Huggingface parameters for load dataset.
         chunk_size (int): The size of the chunks to push the dataset in.
 
     Returns:
         str: The URL to view the data
     """
+    if isinstance(dataset, str):
+        logger.info(f"Downloading dataset {dataset} from huggingface")
+        hf_params = {} or hf_params
+        dataset = load_dataset(dataset, **hf_params)
+
     client = Client(hosted=True)
     uid = str(uuid.uuid4())
     metadata = {
