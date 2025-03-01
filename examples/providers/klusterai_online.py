87a30d6a9efa0c071f154ffe3567ce64080fa723
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2025-03-01 00:56:56 +0000

    Revert "better output token default than 0, happen in kluster deepseek testing, it dump all request at the same time since 0 output completion tokens"
    This reverts commit 0b323e866b6982e739b8ad788019929bcf2cedbc.

--- examples/providers/klusterai_online.py
+++ examples/providers/klusterai_online.py
@@ -7,5 +7,5 @@ print(response["response"])
 
 llm = curator.LLM(model_name="deepseek-ai/DeepSeek-R1", backend="klusterai", backend_params={"max_retries": 1})
 
-response = llm(["What is the capital of Italy?"] * 10)
+response = llm("What is the capital of Italy?")
 print(response["response"])
