3e3241e66760eb17b88c46689d6b76ea3e2ddfc3
sarthakwer <142551729+sarthakwer@users.noreply.github.com>
2025-03-31 00:48:56 -0700

    mistral batch example
    updating how to use mistral batch

--- examples/providers/mistral_batch.py
+++ examples/providers/mistral_batch.py
@@ -1,4 +1,20 @@
-
 import os
 
 from bespokelabs import curator
+
+# To visualize the dataset on Curator viewer, you can set CURATOR_VIEWER=1 environment variable, or set it here:
+# os.environ["CURATOR_VIEWER"]="1"
+
+
+llm = curator.LLM(model_name="mistral-tiny", batch=True)
+questions = [
+    "What is the capital of Montana?",
+    "Who wrote the novel 'Pride and Prejudice'?",
+    "What is the largest planet in our solar system?",
+    "In what year did World War II end?",
+    "What is the chemical symbol for gold?",
+]
+ds = llm(questions)
+
+# Check the first response
+print(ds[0])
