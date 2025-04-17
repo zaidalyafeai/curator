7ec07e2d446736e01dbd0b335b39ac725e603d40
zaidalyafeai <alyafey22@gamil.com>
2025-04-17 11:04:18 +0300

    generalize download data script for english

--- examples/custom/download_data.py
+++ examples/custom/download_data.py
@@ -4,9 +4,16 @@ import argparse
 from tqdm import tqdm
 args = argparse.ArgumentParser()
 args.add_argument("--num-examples", type=int, default=1000)
+args.add_argument("--language", type=str, default="arb-Arab")
+
 args = args.parse_args()
 
-dataset = load_dataset("HuggingFaceFW/fineweb-2", name="arb_Arab", split="train", streaming=True)
+if args.language == "arb-Arab":
+    dataset = load_dataset("HuggingFaceFW/fineweb-2", name=args.language, split="train", streaming=True)
+elif args.language == "eng-Latin":
+    dataset = load_dataset("HuggingFaceFW/fineweb", name="CC-MAIN-2024-10", split="train", streaming=True)
+else:
+    raise ValueError(f"Language {args.language} not supported")
 
 dataset = dataset.take(args.num_examples)
 
@@ -15,11 +22,11 @@ for example in tqdm(dataset):
     examples.append(example)
 
 # save the examples to a json file
-with open(f"fineweb-2-arb-Arab-{args.num_examples}.json", "w") as f:
+with open(f"fineweb-2-{args.language}-{args.num_examples}.json", "w") as f:
     json.dump(examples, f)
 
 
 # load the examples from the json file using datasets
-dataset = load_dataset("json", data_files=f"fineweb-2-arb-Arab-{args.num_examples}.json")
+dataset = load_dataset("json", data_files=f"fineweb-2-{args.language}-{args.num_examples}.json")
 print(dataset)
 
